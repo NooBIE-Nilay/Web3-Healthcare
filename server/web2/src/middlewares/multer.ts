af2c1703db3ddb1dc0c4c9dfb65b893f3203e7c8
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-03-25 20:14:57 +0530

    Added Cors and updated middleware logic

--- server/web2/src/middlewares/multer.ts
+++ server/web2/src/middlewares/multer.ts
@@ -1,14 +1,28 @@
 import multer from "multer";
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    if (!file) {
+      return cb(new Error("No file provided"), "");
+    }
     cb(null, "./public/temp/");
   },
   filename: function (req, file, cb) {
+    if (!file) {
+      return cb(new Error("No file provided"), "");
+    }
     cb(null, file.originalname);
   },
 });
 
 export const upload = multer({
   storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // Limit file size to 5MB
+  },
+  fileFilter: function (req, file, cb) {
+    if (!file) {
+      return cb(new Error("No file provided"));
+    }
+    cb(null, true);
+  },
 });
