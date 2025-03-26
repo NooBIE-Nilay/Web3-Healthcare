363ebfbff1b46bb3c34af9c7f6e62f669daeb74b
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-03-26 21:55:42 +0530

    Added Hardcoded download endpoint

--- server/web2/src/index.ts
+++ server/web2/src/index.ts
@@ -4,6 +4,8 @@ import { upload } from "./middlewares/multer";
 import { uploadOnCloudinary } from "./utils/cloudinary";
 import cors from "cors";
 import fs from "fs";
+import axios from "axios";
+import { generateHashFromFile } from "./utils/generateHashFromFile";
 
 const app = express();
 app.use(cors());
@@ -15,7 +17,6 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   //TODO: Login Logic
-  //requires OTP for verification
 });
 
 app.get("/signup", (req, res) => {
@@ -31,6 +32,7 @@ app.get("/requestAccess", (req, res) => {
 app.get("/viewRequests", (req, res) => {
   // TODO: Patient views all requests =>{public key[]}
 });
+
 try {
   app.post(
     "/upload",
@@ -39,8 +41,8 @@ try {
       //@ts-ignore
       const recordLocalPath = req.files?.record[0]?.path;
       if (!recordLocalPath) res.status(404).json({ msg: "Record Not Found" });
-      const encryptedFile = await encryptFile(recordLocalPath);
       //encrypt file
+      const encryptedFile = await encryptFile(recordLocalPath);
       const cloudRecordObj = await uploadOnCloudinary(encryptedFile.filePath);
       if (!cloudRecordObj) {
         res.status(404).json({ msg: "Record Upload Unsuccessful" });
@@ -49,7 +51,7 @@ try {
       //TODO: DB Entry {Record Link, Record Name, Date, Dr Name}
       //TODO: Send Tx To Blockchain with the RecordID from DB and hash from encryptedFile.hash
       res.json({ msg: "Record Upload Successful", hash: encryptedFile.hash });
-      console.log("Bucket URL:", cloudRecordObj.url);
+      console.log("Bucket Secure URL:", cloudRecordObj.secure_url);
       console.log("Hash:", encryptedFile.hash);
       fs.unlinkSync(recordLocalPath);
     }
@@ -57,6 +59,21 @@ try {
 } catch (e) {
   console.log(e);
 }
-app.get("/download", (req, res) => {});
+app.get("/download", async (req, res) => {
+  try {
+    // Find RecordURL from RecordId, check perms, get hash, and then download and decrypt PDF and verify hash and then send PDF to user.
+    const URL =
+      "https://res.cloudinary.com/db2vbqbhn/image/upload/v1742969968/ov7wkfjg5jdbvlxthjwx.pdf";
+    const response = await axios.get(URL, {
+      responseType: "arraybuffer",
+    });
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", 'attachment; filename="download.pdf"');
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching PDF:", error);
+    res.status(500).json({ error: "Failed to fetch PDF" });
+  }
+});
 
 app.listen(8080, () => console.log("Server Runnning in Port 8080"));
--- server/web2/src/utils/cloudinary.ts
+++ server/web2/src/utils/cloudinary.ts
@@ -1,10 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 require("dotenv").config();
 import fs from "fs";
+const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+const api_key = process.env.CLOUDINARY_API_KEY;
+const api_secret = process.env.CLOUDINARY_API_SECRET;
+console.log(api_key, api_secret, cloud_name);
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name,
+  api_key,
+  api_secret,
+  secure: true,
 });
 export const uploadOnCloudinary = async (localFilePath: string) => {
   try {
